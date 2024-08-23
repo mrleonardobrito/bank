61730bc43113e5c415aceea70c74b2cc894f91e8
mrleonardobrito <lbritogit@gmail.com>
2024-08-22 23:01:52 -0300

    refactoring things

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,15 +1,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "../cliente/cliente.h" 
+#include "../node/node.h"  
+#include "../report/report.h" 
+
 #ifdef _WIN32
 # define CLEAR_SCREEN system("cls")
 #else
 # define CLEAR_SCREEN puts("\x1b[H\x1b[2J")
 #endif
-
-#include "../cliente/cliente.h" 
-#include "../node/node.h"  
-#include "../report/report.h" 
 
 void limpar_buffer();
 void menu(Node* fila);
